7c91d5b6d18cf2bd525dc9496a4e86ca238976c7
subashinvest277-max <subashinvest277@gmail.com>
2026-06-17 18:35:54 +0530

    Update start.sh

--- Job-Portal-Project-Dev/start.sh
+++ Job-Portal-Project-Dev/start.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
+set -e
+
 echo "Running migrations"
 
 python manage.py migrate
 
 
-echo "Collecting static files"
-
-python manage.py collectstatic --noinput
-
-
 echo "Starting Gunicorn"
-
 
 gunicorn jobportal.wsgi:application \
 --bind 127.0.0.1:8000 \
 --workers 3 &
 
 
-
 echo "Starting Nginx"
-
 
 nginx -g "daemon off;"
